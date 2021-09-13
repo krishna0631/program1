d51515b40ce85efc2a71f594d3f49ba8bf97a41c
krishna0631 <78727062+krishna0631@users.noreply.github.com>
2021-09-13 20:37:43 +0530

    m

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    print("Addition:",a+b)
+def sub(a,b):
+    print("Subtraction:",a-b)
+def mul(a,b):
+    print("Multiplication:",a*b)
+def div(a,b):
+    print("Division:",a/b)
+def mod(a,b):
+    print("Modulus:",a%b)
---
+++ sample.py
@@ -0,0 +1,6 @@
+from calculator import add,sub,mul,div,mod
+add(5,5)
+sub(10,5)
+mul(8,8)
+div(3,9)
+mod(9,10)
